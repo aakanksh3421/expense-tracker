db686dfb9cbe0462cec78c0f71e6e8ca07979955
aakanksh3421 <120978450+aakanksh3421@users.noreply.github.com>
2024-03-11 23:46:33 +0530

    second

--- src/components/Spend_history.jsx
+++ src/components/Spend_history.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Progressbar from './progressbar'; 
  import "./Home.css"
 
 export default function Spend_history() {
